c0847b307ef666b3a151d8d36d2de545462147dc
vojta <kuzdavoj@fel.cvut.cz>
2024-03-14 11:30:26 +0100

    printTestNameToConsole

--- src/test/java/UtilTest.java
+++ src/test/java/UtilTest.java
@@ -1,22 +1,25 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInfo;
 import pastry.Util;
 
+import java.lang.reflect.Method;
+
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class UtilTest {
+
+    @BeforeEach
+    void printTestNameToConsole(TestInfo testInfo) {
+        System.out.println(System.lineSeparator() + System.lineSeparator()
+                + "============== " + testInfo.getTestMethod().map(Method::getName).orElse(null)
+                + "() =============" + System.lineSeparator());
+    }
 
     @Test
     public void testGetId() {
         String seznam = "77.75.77.222:80";
         long id = Util.getDistance(seznam);
-        assertNotEquals(Long.MAX_VALUE, id);
-    }
-
-    @Test
-    public void testGetId_google() {
-        String google = "142.251.36.110:80";
-        long id = Util.getDistance(google);
         assertNotEquals(Long.MAX_VALUE, id);
     }
 
