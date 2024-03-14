6772fb923ceb1a38c83146b834889ebec4f9aa3d
vojta <kuzdavoj@fel.cvut.cz>
2024-03-14 11:36:17 +0100

    logger in CI test

--- src/test/java/UtilTest.java
+++ src/test/java/UtilTest.java
@@ -1,6 +1,8 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInfo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import pastry.Util;
 
 import java.lang.reflect.Method;
@@ -8,6 +10,8 @@ import java.lang.reflect.Method;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 public class UtilTest {
+
+    Logger logger = LoggerFactory.getLogger(UtilTest.class);
 
     @BeforeEach
     void printTestNameToConsole(TestInfo testInfo) {
@@ -18,6 +22,7 @@ public class UtilTest {
 
     @Test
     public void testGetId() {
+        logger.warn("logger in CI test");
         String seznam = "77.75.77.222:80";
         long id = Util.getDistance(seznam);
         assertNotEquals(Long.MAX_VALUE, id);
