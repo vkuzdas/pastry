2fbc9f7e9869a07ad4554d1f9dc48ae2134bf9c4
vojta <kuzdavoj@fel.cvut.cz>
2024-03-14 11:42:20 +0100

    logger.warn("logger in CI test"); put back

--- src/test/java/UtilTest.java
+++ src/test/java/UtilTest.java
@@ -22,6 +22,7 @@ public class UtilTest {
 
     @Test
     public void testGetId() {
+        logger.warn("logger in CI test");
         String seznam = "77.75.77.222:80";
         long id = Util.getDistance(seznam);
         assertNotEquals(Long.MAX_VALUE, id);
