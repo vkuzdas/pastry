a20b78c014da7bee93b82bbf4b76b9e272dced1b
vkuzdas <kuzdavoj@fel.cvut.cz>
2024-03-31 09:11:27 +0200

    disable some hex tests to see which is unstable

--- src/test/java/RecoveryHexTest.java
+++ src/test/java/RecoveryHexTest.java
@@ -1,9 +1,11 @@
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import pastry.PastryNode;
 
 import static pastry.Constants.BASE_16_IDS;
 import static pastry.Constants.LEAF_SET_SIZE_32;
 
+@Disabled
 public class RecoveryHexTest extends RecoveryTest {
 
         @Override
